a9908151ad6f4daf846e5e6b0b5643a8ff9287e9
Sneha_148 <snehasingh10921@gmail.com>
2026-04-07 01:37:31 +0530

    Add recursive Fibonacci series function
    Implement Fibonacci series using recursion.

--- fibonacci_recursion.py
+++ fibonacci_recursion.py
@@ -1,1 +1,12 @@
-
+def fibbonacci_series(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    else:
+        series = fibbonacci_series(n - 1)
+        series.append(series[-1] + series[-2])
+        return series
+print(fibbonacci_series(11))
