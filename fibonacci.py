175fdeb44f5563de0e1f5f03310b737a1d37bc05
Sneha_148 <snehasingh10921@gmail.com>
2026-04-07 01:38:07 +0530

    Add function to generate Fibonacci sequence

--- fibonacci.py
+++ fibonacci.py
@@ -1,1 +1,9 @@
+def fibonacci_sequence(n):
+    sequence = []
+    a, b = 0, 1
+    for _ in range(n):
+        sequence.append(a)
+        a, b = b, a + b
+    return sequence
 
+print(fibonacci_sequence(10))
